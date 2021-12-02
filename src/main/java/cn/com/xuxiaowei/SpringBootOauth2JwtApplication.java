3343de1febb4c14efdf73d68c936f652aff05b52
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-12-02 14:54:45 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootOauth2JwtApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2JwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/SpringBootOauth2JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
